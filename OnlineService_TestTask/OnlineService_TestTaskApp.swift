359f67e8b378abc08fffe51670c30311cca3bf03
Polina Skuratova <skuratova250496@gmail.com>
2022-12-17 18:39:48 +0700

    Initial Commit

---
+++ OnlineService_TestTask/OnlineService_TestTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineService_TestTaskApp.swift
+//  OnlineService_TestTask
+//
+//  Created by Поляндий on 17.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct OnlineService_TestTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
